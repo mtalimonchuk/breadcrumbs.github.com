c52a7a446da78ac1221b9cf91f1cf2bd5bf9d471
Myhailo Talimonchuk <mychaylo.talimonchuk@iteam-pro.com>
2015-04-20 16:45:21 +0300

    Breadcrumbs

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+//phpinfo();
+?>
+
+<link href="source/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<link href="source/css/style.css" rel="stylesheet">
+
+<script src="source/js/jquery-1.11.2.js"></script>
+<script src="source/js/jquery.breadcrumb.js"></script>
+<script src="source/bootstrap/js/bootstrap.min.js"></script>
+
+<div class="bread">
+  <ol class="bread-crumb breadcrumb">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Products</a></li>
+    <li><a href="#">Category</a></li>
+    <li><a href="#">Product-name</a></li>
+    <li><a href="#">Product-filter-price</a></li>
+    <li><a href="#">about</a></li>
+    <li><a href="#">tedtdfsd</a></li>
+  </ol>
+</div>
+
+<script>$('.bread-crumb').mfcrumb({divider: '|',windowClass: 'bread',animationTime: 100,animation: true});</script>
